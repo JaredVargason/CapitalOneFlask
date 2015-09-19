e2c1ecb45c615a81f9bdd1d1befa2326e6235774
jared <jv20280@gmail.com>
2015-09-19 15:45:21 -0400

    dinner

--- app/run.py
+++ app/run.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from config import CAPITAL_ONE_KEY
+from pymongo import MongoClient
+import json
 import requests
 
 app = Flask(__name__)
+client = MongoClient()
+db = client.test
 
 @app.route('/')
 def index():
@@ -15,7 +19,12 @@ def bank():
 @app.route('/merchant')
 def get_merchant():
 	r = requests.get("http://api.reimaginebanking.com/merchants?key=b3ecb3d19e91b8fa639d3f798dbbfaca")
-	return jsonify(r)
+	return json.dumps(r.text)
+
+@app.route('/get_user_transactions')
+def get_user_transactions():
+	#Get account IDs, customer names from customer (get customer by account ID), purchases on account, and the locations of the purchases from the merchant. money spent, etc.
+	r = requests.get("http://")
 
 
 if __name__ == '__main__':
