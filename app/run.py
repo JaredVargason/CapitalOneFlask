7e57508f0aec3ff2c64aafa85f28ba96c4b4b8cd
jared <jv20280@gmail.com>
2015-09-20 01:09:04 -0400

    asdfaf

--- app/run.py
+++ app/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from config import CAPITAL_ONE_KEY
 from pymongo import MongoClient
 from bson.json_util import dumps
@@ -29,16 +29,22 @@ def get_merchant():
 	r = requests.get("http://api.reimaginebanking.com/merchants?key=b3ecb3d19e91b8fa639d3f798dbbfaca")
 	return json.dumps(r.text)
 
-@app.route('/get_user_transaction_info', methods=['POST', 'GET'])
-def get_user_transactions():
+@app.route('/get_user_purchase_info', methods=['POST', 'GET'])
+def get_user_purchase_info():
 	#Get account IDs, customer names from customer (get customer by account ID), purchases on account, and the locations of the purchases from the merchant. money spent, etc.
+	account_id = request.form['account_id']
+	print account_id
 	Purchase = db.Purchase
-	cursor = Purchase.find()
-	'''for item in cursor:
-		if item['payer_id'] =='hi':
-			return "bye"'''
+	cursor = Purchase.find({'payer_id':str(account_id)})
+	print str(cursor.count())
+	for item in cursor:
+		print item
+		doc = Purchase.find_one({'merchant_id' : item['merchant_id']})
+		item['geocode']['lat'] = doc['geocode']['lat']
+		item['geocode']['lng'] = doc['geocode']['lng']
+		item['name'] = doc['name']
 
-	return "hi"
+	return dumps(cursor)
 
 @app.route('/get_branches', methods=['GET'])
 def get_branches():
--- app/static/customer.js
+++ app/static/customer.js
@@ -1,51 +1,3 @@
-/*function initialize() {
-  var data = [{
-    "title": "Stockholm",
-    "lat": 59.3,
-    "lng": 18.1,
-    "description": "Stockholm is the capital and the largest city of Sweden and constitutes the most populated urban area in Scandinavia with a population of 2.1 million in the metropolitan area (2010)"
-  },
-  {
-    "title": "Oslo",
-    "lat": 59.9,
-    "lng": 10.8,
-    "description": "Oslo is a municipality, and the capital and most populous city of Norway with a metropolitan population of 1,442,318 (as of 2010)."
-  },
-  {
-    "title": "Copenhagen",
-    "lat": 55.7,
-    "lng": 12.6,
-    "description": "Copenhagen is the capital of Denmark and its most populous city, with a metropolitan population of 1,931,467 (as of 1 January 2012)."
-  }];
-
-    var mapCanvas = document.getElementById('map');
-
-  var mapOptions = {
-    center: new google.maps.LatLng(44.5403, -78.5463),
-    zoom: 2,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-    console.log("made it here");
-
-    var myLatLng = {lat: 45.363, lng: -72.044};
-    var cityCircle = new google.maps.Circle({
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35,
-      map: map,
-      center: {lat: 42.714, lng: -75.000},
-      radius: 10000
-    });
-};
-
-console.log("happened");
-google.maps.event.addDomListener(window, 'load', initialize);
-console.log("done");*/
-
 function initialize() {
     var mapCanvas = document.getElementById('map');
 
@@ -56,21 +8,8 @@ function initialize() {
   };
 
     map = new google.maps.Map(mapCanvas, mapOptions);
-    var myLatLng = {lat: 45.363, lng: -72.044};
-    var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
-  })
 
-    myLatLng = {lat: 36.363, lng: -35.044}
-    var marker2 = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
-  });
-
-  yes();
+  //yes();
 };
 
 var map = null;
@@ -79,14 +18,18 @@ var markers = [];
 google.maps.event.addDomListener(window, 'load', initialize);
 
 function yes() {
+  console.log('ok')
+  payload = {'account_id' : $('#superid').val()}
+
   //Send the AJAX call to the server
   $.ajax({
   //The URL to process the request
-    'url' : 'http://localhost:5000/get_user_transaction_info',
+    'url' : 'http://localhost:5000/get_user_purchase_info',
   //The type of request, also known as the "method" in HTML forms
   //Can be 'GET' or 'POST'
-    'type' : 'GET',
-    'datatype' : 'text/xml',
+    'type' : 'POST',
+    'data' : payload,
+    'datatype' : 'application/json',
   //The response from the server
     'success' : function(data) {
     //You can use any jQuery/JavaScript here!!!
@@ -94,17 +37,23 @@ function yes() {
 
       swag.forEach(function(o) {
         
-                 /*if (typeof data.geocode === undefined || typeof data.geocode.lat === undefined || typeof data.gecode.lng === undefined) {
-            return;}*/
+            contentString='<h1>' + o.name + '</h1>'
 
             if (o.geocode.lat && o.geocode.lng) {
+                var infowindow = new google.maps.InfoWindow({
+               content: contentString
+                });
 
-              markers.push(new google.maps.Marker({
+                marker = new google.maps.Marker({
                 position: {lat: o.geocode.lat, lng: o.geocode.lng},
                 map: map,
                 title: o._id
-              }))}
+              })
 
+                marker.addListener('click', function() {
+                infowindow.open(map, marker);
+                });
+              }
       });
     }
   });
